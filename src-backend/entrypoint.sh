0ac96e623b33d96eff21715c82e4e31e34129d13
SURESH BATTA <61498234+Sureshgh197@users.noreply.github.com>
2022-11-18 13:42:20 +0530

    Update entrypoint.sh

--- src-backend/entrypoint.sh
+++ src-backend/entrypoint.sh
@@ -3,4 +3,4 @@
 python manage.py migrate --no-input
 # python manage.py collectstatic --no-input
 # gunicorn project.wsgi:application --bind 0.0.0.0:80
-python manage.py runserver 80
+python manage.py runserver 0.0.0.0:80
